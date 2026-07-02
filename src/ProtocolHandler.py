1dc7ac5921b50ae599714f3dc3937e40e65baae6
Tigran <tigran.wattrelos@gmail.com>
2026-07-02 16:09:25 +0200

    Add wrtting part of handler

--- src/ProtocolHandler.py
+++ src/ProtocolHandler.py
@@ -1,4 +1,6 @@
 # We'll use exceptions to notify the connection-handling loop of problems.
+from io import BytesIO
+
 from Server import Disconnect, CommandError, Error
 
 class ProtocolHandler(object):
@@ -52,5 +54,32 @@ class ProtocolHandler(object):
         
 
     def write_response(self, socket_file, data):
-        # Serialize the response data and send it to the client.
-        pass
+        buf = BytesIO()
+        self._write(buf, data)
+        buf.seek(0)
+        socket_file.write(buf.getvalue())
+        socket_file.flush()
+
+    def _write(self, buf, data):
+        if isinstance(data, str):
+            data = data.encode('utf-8')
+
+        if isinstance(data, bytes):
+            buf.write('$%s\r\n%s\r\n' % (len(data), data))
+        elif isinstance(data, int):
+            buf.write(':%s\r\n' % data)
+        elif isinstance(data, Error):
+            buf.write('-%s\r\n' % data.message)  ## Pas sur de ce data.message
+        elif isinstance(data, (list, tuple)):
+            buf.write('*%s\r\n' % len(data))
+            for item in data:
+                self._write(buf, item)
+        elif isinstance(data, dict):
+            buf.write('%%%s\r\n' % len(data))
+            for key in data:
+                self._write(buf, key)
+                self._write(buf, data[key])
+        elif data is None:
+            buf.write('$-1\r\n')
+        else:
+            raise CommandError('unrecognized type: %s' % type(data))
