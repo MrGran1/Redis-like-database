969647673f34ad5f0b5d4af1d484c58479b232b5
Tigran <tigran.wattrelos@gmail.com>
2026-07-02 15:45:16 +0200

    Separation of ProtocolHandler and Server

--- src/Server.py
+++ src/Server.py
@@ -15,9 +15,20 @@ Error = namedtuple('Error', ('message',))
 
 
 class ProtocolHandler(object):
+    def __init__(self):
+        self.handlers = {
+            '+': self.handle_simple_string,
+            '-': self.handle_error,
+            ':': self.handle_integer,
+            '$': self.handle_string,
+            '*': self.handle_array,
+            '%': self.handle_dict
+            }
+
     def handle_request(self, socket_file):
         # Parse a request from the client into it's component parts.
         pass
+        
 
     def write_response(self, socket_file, data):
         # Serialize the response data and send it to the client.
